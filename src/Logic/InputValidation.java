fb4fdf77428e81c507a4c38c03e4dfc89df6bbaa
qiang <qiangloozen@gmail.com>
2021-01-17 19:40:05 +0100

    Postalcode Jtests working except for lowercase

--- src/Logic/InputValidation.java
+++ src/Logic/InputValidation.java
@@ -2,10 +2,10 @@ package Logic;
 
 import java.sql.Date;
 
-public class InputValidation{
+public class InputValidation {
 
-    // Mail validation 
-    public static boolean validateMailAddress(String mailAddress){
+    // Mail validation
+    public static boolean validateMailAddress(String mailAddress) {
         if (!mailAddress.contains("@") || mailAddress.split("@")[0].length() < 1) {
             return false;
         }
@@ -21,25 +21,25 @@ public class InputValidation{
         if (!mailAddress.contains("@") || mailAddress.split("@")[1].split("\\.", -1)[1].length() < 1) {
             return false;
         }
-        
+
         return true;
     }
 
     // URL validation
-    public static boolean validateUrl(String url){
-        if(!url.startsWith("https://") || url.startsWith("http://")){
+    public static boolean validateUrl(String url) {
+        if (!url.startsWith("https://") || url.startsWith("http://")) {
             return false;
         }
 
-        if (url.split("/", 5)[1].split("\\.")[0].length() < 1){
+        if (url.split("/", 5)[1].split("\\.")[0].length() < 1) {
             return false;
         }
 
-        if (url.split("\\.")[1].split("\\.")[0].length() < 1){
+        if (url.split("\\.")[1].split("\\.")[0].length() < 1) {
             return false;
         }
 
-        if (url.split("\\.")[1].split("\\.", -1)[1].length() < 1){
+        if (url.split("\\.")[1].split("\\.", -1)[1].length() < 1) {
             return false;
         }
 
@@ -47,8 +47,8 @@ public class InputValidation{
     }
 
     // Percentages validation
-    public static boolean validatePercentage(int percentage){
-        if (percentage < 0 || percentage > 100){
+    public static boolean validatePercentage(int percentage) {
+        if (percentage < 0 || percentage > 100) {
             return false;
         }
 
@@ -56,14 +56,14 @@ public class InputValidation{
     }
 
     // Date validation
-    public static boolean validateDate(Date date){
-        
+    public static boolean validateDate(Date date) {
+
         return true;
     }
 
     // Certificate grade validation
-    public static boolean validateGrade(double grade){
-        if (grade < 1 || grade > 10){
+    public static boolean validateGrade(double grade) {
+        if (grade < 1 || grade > 10) {
             return false;
         }
 
@@ -71,22 +71,47 @@ public class InputValidation{
     }
 
     // Zip code/ Postal code validation
-    public static boolean validatePostalCode(String zipCode){
-        if (zipCode.length() != 6){
+    public static boolean validatePostalCode(String zipCode) {
+        // @subcontract null postalCode
+        if (zipCode.equals(null)) {
             return false;
         }
-        if (zipCode.charAt(0) > 0 && zipCode.charAt(0) < 11){
-            for (int i = 0; i < 4; i++) {
-                if (!Character.isDigit(zipCode.charAt(i))){
-                    return false;
-                }
+
+        // @subcontract valid postalCode
+        try {
+            int zipCodeNumbers = Integer.valueOf(zipCode.trim().substring(0, 4));
+            String zipCodeLetters = zipCode.trim().substring(4).trim();
+            char zipCodeFirstLetter = zipCode.trim().substring(4).trim().toUpperCase().charAt(0);
+            char zipCodeSecondLetter = zipCode.trim().substring(4).trim().toUpperCase().charAt(1);
+
+            if (zipCodeNumbers > 999 && zipCodeNumbers <= 9999 && zipCodeLetters.length() == 2
+                    && ('A' <= zipCodeFirstLetter && zipCodeFirstLetter <= 'Z')
+                    && ('A' <= zipCodeSecondLetter && zipCodeSecondLetter <= 'Z')) {
+                // If postalcode is valid, return true
+                return true;
             }
-            for (int i = 4; i < 6; i++) {
-                if (!Character.isLetter(zipCode.charAt(i))){
-                    return false;
-                }
-            }
+
+        } catch (Exception e) {
+            // @subcontract invalid postalCode
+            return false;
         }
-        return true;
+
+        // if (zipCode.length() != 6){
+        // return false;
+        // }
+        // if (zipCode.charAt(0) > 0 && zipCode.charAt(0) < 11){
+        // for (int i = 0; i < 4; i++) {
+        // if (!Character.isDigit(zipCode.charAt(i))){
+        // return false;
+        // }
+        // }
+        // for (int i = 4; i < 6; i++) {
+        // if (!Character.isLetter(zipCode.charAt(i))){
+        // return false;
+        // }
+        // }
+        // }
+
+        return false;
     }
 }
--- src/Logic/Tests/PostalCodeValidationTest.java
+++ src/Logic/Tests/PostalCodeValidationTest.java
@@ -1,70 +1,185 @@
 package Logic.Tests;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.Test;
 import Logic.InputValidation;
 
-
 public class PostalCodeValidationTest {
 
-    @Test
-    public void testValidatePostalCodeCanBeShorterThen6CharactersEnsuresFalse(){
+    // * @subcontract null postalCode {
+    // * @requires postalCode == null;
+    // * @signals (NullPointerException) postalCode == null;
+    @Test(expected = NullPointerException.class)
+    public void testValidatePostalCodeRequiresNullSignalsNullPointerException() {
         // Arrange
-        String PostalCode = "1234A";
-
+        String PostalCode = null;
         // Act
         Boolean result = InputValidation.validatePostalCode(PostalCode);
+    }
 
+    // @subcontract valid postalCode
+    @Test
+    public void testValidatePostalCodeRequires1000AAEnsuresTrue() {
+        // Arrange
+        String PostalCode = "1000 AA";
+        // Act
+        Boolean result = InputValidation.validatePostalCode(PostalCode);
         // Assert
-        assertEquals(false, result);
+        assertTrue("This is a valid zipcode", result);
     }
 
     @Test
-    public void testValidatePostalCodeCanBeLongerThen6CharactersEnsuresFalse(){
+    public void testValidatePostalCodeRequires9999ZZEnsuresTrue() {
         // Arrange
-        String PostalCode = "1234AAA";
-
+        String PostalCode = "9999ZZ";
         // Act
         Boolean result = InputValidation.validatePostalCode(PostalCode);
-
         // Assert
-        assertEquals(false, result);
-    }
-
-
-    @Test
-    public void testValidatePostalCodeStartsWith0EnsuresFalse(){
-        // Arrange
-        String PostalCode = "0123AB";
-
-        // Act
-        Boolean result = InputValidation.validatePostalCode(PostalCode);
-
-        // Assert
-        assertEquals(false, result);
+        assertTrue("This is a valid zipcode", result);
     }
 
     @Test
-    public void testValidatePostalCodeDoesNotStartWith4DigitsEnsuresFalse(){
+    public void testValidatePostalCodeRequires5000LLEnsuresTrue() {
         // Arrange
-        String PostalCode = "123ABC";
-
+        String PostalCode = "5000 LL";
         // Act
         Boolean result = InputValidation.validatePostalCode(PostalCode);
-
         // Assert
-        assertEquals(false, result);
+        assertTrue("This is a valid zipcode", result);
+    }
+
+    // @subcontract invalid postalCode
+    @Test
+    public void testValidatePostalCodeRequires999AAEnsuresFalse() {
+        // Arrange
+        String PostalCode = "999 AA";
+        // Act
+        Boolean result = InputValidation.validatePostalCode(PostalCode);
+        // Assert
+        assertFalse("This is a invalid zipcode", result);
     }
 
     @Test
-    public void testValidatePostalCodeDoesNotEndWith2LettersEnsuresFalse(){
+    public void testValidatePostalCodeRequires10000AAEnsuresFalse() {
         // Arrange
-        String PostalCode = "12345A";
-
+        String PostalCode = "10000 AA";
         // Act
         Boolean result = InputValidation.validatePostalCode(PostalCode);
-
         // Assert
-        assertEquals(false, result);
+        assertFalse("This is a invalid zipcode", result);
     }
+
+    @Test
+    public void testValidatePostalCodeRequires333LLEnsuresFalse() {
+        // Arrange
+        String PostalCode = "333 LL";
+        // Act
+        Boolean result = InputValidation.validatePostalCode(PostalCode);
+        // Assert
+        assertFalse("This is a invalid zipcode", result);
+    }
+
+    @Test
+    public void testValidatePostalCodeRequires1000AEnsuresFalse() {
+        // Arrange
+        String PostalCode = "1000 A";
+        // Act
+        Boolean result = InputValidation.validatePostalCode(PostalCode);
+        // Assert
+        assertFalse("This is a invalid zipcode", result);
+    }
+
+    @Test
+    public void testValidatePostalCodeRequires1000spaceAEnsuresFalse() {
+        // Arrange
+        String PostalCode = "1000  A";
+        // Act
+        Boolean result = InputValidation.validatePostalCode(PostalCode);
+        // Assert
+        assertFalse("This is a invalid zipcode", result);
+    }
+
+    // This test is not working
+    @Test
+    public void testValidatePostalCodeRequires1000aaEnsuresFalse() {
+        // Arrange
+        String PostalCode = "1000 aa";
+        // Act
+        Boolean result = InputValidation.validatePostalCode(PostalCode);
+        // Assert
+        assertFalse("This is a invalid zipcode", result);
+    }
+
+    @Test
+    public void testValidatePostalCodeRequires1000SpecialcharEnsuresFalse() {
+        // Arrange
+        String PostalCode = "1000 %#";
+        // Act
+        Boolean result = InputValidation.validatePostalCode(PostalCode);
+        // Assert
+        assertFalse("This is a invalid zipcode", result);
+    }
+
+    // @Test
+    // public void
+    // testValidatePostalRequiresCodeCanBeShorterThen6CharactersEnsuresFalse() {
+    // // Arrange
+    // String PostalCode = "1234A";
+
+    // // Act
+    // Boolean result = InputValidation.validatePostalCode(PostalCode);
+
+    // // Assert
+    // assertEquals(false, result);
+    // }
+
+    // @Test
+    // public void testValidatePostalCodeCanBeLongerThen6CharactersEnsuresFalse() {
+    // // Arrange
+    // String PostalCode = "1234AAA";
+
+    // // Act
+    // Boolean result = InputValidation.validatePostalCode(PostalCode);
+
+    // // Assert
+    // assertEquals(false, result);
+    // }
+
+    // @Test
+    // public void testValidatePostalCodeStartsWith0EnsuresFalse() {
+    // // Arrange
+    // String PostalCode = "0123AB";
+
+    // // Act
+    // Boolean result = InputValidation.validatePostalCode(PostalCode);
+
+    // // Assert
+    // assertEquals(false, result);
+    // }
+
+    // @Test
+    // public void testValidatePostalCodeDoesNotStartWith4DigitsEnsuresFalse() {
+    // // Arrange
+    // String PostalCode = "123ABC";
+
+    // // Act
+    // Boolean result = InputValidation.validatePostalCode(PostalCode);
+
+    // // Assert
+    // assertEquals(false, result);
+    // }
+
+    // @Test
+    // public void testValidatePostalCodeDoesNotEndWith2LettersEnsuresFalse() {
+    // // Arrange
+    // String PostalCode = "12345A";
+
+    // // Act
+    // Boolean result = InputValidation.validatePostalCode(PostalCode);
+
+    // // Assert
+    // assertEquals(false, result);
+    // }
 }
