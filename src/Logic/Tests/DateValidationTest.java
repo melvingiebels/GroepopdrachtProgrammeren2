cbfb8a1c97f9661c6e19d3cd7c733f58d886cb6c
Pieter Bakker <18117805@student.hhs.nl>
2021-01-21 15:49:09 +0100

    Added dateValidation + tests
    date validation can now be done with  a single string created from the 3 date textfields

--- src/Logic/Tests/DateValidationTest.java
+++ src/Logic/Tests/DateValidationTest.java
@@ -4,9 +4,113 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import Logic.Validation.MailValidation;
+
+import Logic.Validation.DateValidation;
 
 
 public class DateValidationTest {
+    // @subcontract == 31 days in month    
+    @Test
+    public void testValidateDateRequiresMonth1And31DaysEnsuresTrue() {
+        // Arrange
+        String date = "31-01-2019";
+
+        // Act
+        Boolean result = DateValidation.validateDate(date);
+
+        // Assert
+        assertTrue("31 days in given month", result);
+    }  
     
+    // @subcontract > 31 days in month 
+    @Test
+    public void testValidateDateRequiresMonth1And32DaysEnsuresFalse(){
+        // Arrange
+        String date = "32-01-2019";
+
+        // Act
+        Boolean result = DateValidation.validateDate(date);
+
+        // Assert
+        assertFalse("Month contains more than 31 day", result);
+    }
+
+    // @subcontract == 30 days in month
+    @Test
+    public void testValidateDateRequiresMonth4And30DaysEnsuresTrue() {
+        // Arrange
+        String date = "30-04-2020";
+
+        // Act
+        Boolean result = DateValidation.validateDate(date);
+
+        // Assert
+        assertTrue("Month can only contain a maximum of 30 days", result);
+    }
+
+    // @ Subcontract > 30 days in month
+    @Test
+    public void testValidateDateRequiresMonth4And31DaysEnsuresFalse() {
+        // Arrange
+        String date = "31-04-2020";
+
+        // Act
+        Boolean result = DateValidation.validateDate(date);
+
+        // Assert
+        assertFalse("Month can only contains a maximum of 30 days", result);
+    }
+
+
+    // * @subcontract == 29 days in month
+    @Test
+    public void testValidateDateRequiresMonth2And29DaysAndYear2012EnsuresTrue() {
+        // Arrange
+        String date = "29-02-2012";
+
+        // Act
+        Boolean result = DateValidation.validateDate(date);
+
+        // Assert
+        assertTrue("Month does not contain a maximum of 29 days", result);
+    }
+    
+    // * @subcontract > 29 days in month
+    @Test
+    public void testValidateDateRequiresMonth2And30DaysAndYear2012EnsuresFalse() {
+        // Arrange
+        String date = "30-02-2012";
+
+        // Act
+        Boolean result = DateValidation.validateDate(date);
+
+        // Assert
+        assertFalse("Month contains a maximum of 29 days", result);
+    }
+
+    //* @subcontract == 28 days in month
+    @Test
+    public void testValidateDateRequiresMonth2And28DaysAndYear2010EnsuresTrue() {
+        // Arrange
+        String date = "28-02-2010";
+
+        // Act
+        Boolean result = DateValidation.validateDate(date);
+
+        // Assert
+        assertTrue("No Leap year can only contains a maximum of 28", result);
+    }
+
+    //* @subcontract > 28 days in month
+    @Test
+    public void testValidateDateRequiresMonth2And29DaysAndYear2010EnsuresFalse() {
+        // Arrange
+        String date = "29-02-2010";
+
+        // Act
+        Boolean result = DateValidation.validateDate(date);
+
+        // Assert
+        assertFalse("No Leap year can only contains a maximum of 28", result);
+    }
 }
--- src/Logic/Validation/DateValidation.java
+++ src/Logic/Validation/DateValidation.java
@@ -4,6 +4,42 @@ public class DateValidation {
     
     public static boolean validateDate(String date) {
 
+        if (date.length() < 10 && date.length() > 10){
+            return false;
+        }
+        
+        String dateParts[] = date.split("-");
+
+        if (dateParts.length < 3 && dateParts.length > 3){
+            return false;
+        }
+        
+        int day = Integer.valueOf(dateParts[0]);
+        int month = Integer.valueOf(dateParts[1]);
+        int year = Integer.valueOf(dateParts[2]);
+        System.out.println("days: " + day + " month: " + month + " year: " + year);
+
+        //* @subcontract 31 days in month
+        if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && (day < 1 || day > 31)) {
+            System.out.println("first");
+            return false;
+        }
+        // * @subcontract 30 days in month 
+        if ((month == 4 || month == 6 || month == 9 || month == 11) && (day < 1 || day > 30)) {
+            System.out.println("second");
+            return false;
+        }
+        // * @subcontract 29 days in month
+        if (month == 2 && (day < 1 || day > 29) && year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+            System.out.println("third");
+            return false;
+        }
+        // * @subcontract 28 days in month
+        if (month == 2 && (day < 1 || day > 28) && (year % 4 != 0 || (year % 100 == 0 && year % 400 != 0))){
+            System.out.println("fourth");
+            return false;
+        }
+        
         return true;
     }
 }
