368c31efeb930da0d603fd41b6e3478dfca99b3a
Melvin <Melvingiebels@gmail.com>
2021-01-21 12:56:29 +0100

    UpdateContentItemDAO

--- src/Database/ContentItemDAO.java
+++ src/Database/ContentItemDAO.java
@@ -2,6 +2,8 @@ package Database;
 
 import java.sql.*;
 import java.util.ArrayList;
+
+import Domain.Course;
 import Domain.Module;
 
 public class ContentItemDAO extends GenericDAO {
@@ -21,6 +23,25 @@ public class ContentItemDAO extends GenericDAO {
             }
         } catch (Exception e) {
             System.out.println("failed to getAvailableModules");
+        }
+        return modules;
+    }
+
+    public ArrayList<Module> getCourseModules(Course course) {
+        ArrayList<Module> modules = new ArrayList<>();
+        SQL = String.format("SELECT * FROM Module WHERE CourseName='%s'", course.getName());
+
+        try (PreparedStatement stmt = con.prepareStatement(SQL)) {
+            // execute statement
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                modules.add(new Module(rs.getInt("ContentItemId"), rs.getInt("Version"), rs.getString("ContactName"),
+                        rs.getString("ContactMail"), rs.getInt("IndexNumber"), rs.getString("Title"),
+                        rs.getString("CourseName")));
+            }
+        } catch (Exception e) {
+            System.out.println("failed to getCourseModules");
         }
         return modules;
     }
