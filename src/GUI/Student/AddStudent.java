a5cd29267a40b99caa9c8b8a50b3052c72f098c9
qiang <qiangloozen@gmail.com>
2021-01-23 20:47:18 +0100

    Validation for addStudent working

--- src/GUI/Student/AddStudent.java
+++ src/GUI/Student/AddStudent.java
@@ -2,7 +2,9 @@ package GUI.Student;
 
 import Domain.Student;
 import GUI.GenericGUI;
+import Logic.Validation.DateValidation;
 import Logic.Validation.MailValidation;
+import Logic.Validation.NameValidation;
 import Logic.Validation.ZipCodeValidation;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -49,6 +51,7 @@ public class AddStudent extends GenericGUI {
         TextField emailInput = new TextField();
         emailInput.setPromptText("example@email.com");
         TextField nameInput = new TextField();
+        nameInput.setPromptText("Firstname Lastname");
 
         HBox dateInput = new HBox();
         dateInput.setSpacing(20);
@@ -67,9 +70,13 @@ public class AddStudent extends GenericGUI {
         genderInput.getItems().addAll("Male", "Female");
 
         TextField addressInput = new TextField();
+        addressInput.setPromptText("Streetname 123");
         TextField cityInput = new TextField();
+        cityInput.setPromptText("Cityname");
         TextField countryInput = new TextField();
+        countryInput.setPromptText("Countryname");
         TextField zipCodeInput = new TextField();
+        zipCodeInput.setPromptText("1234 AB");
 
         Label emailLabel = new Label("Email:");
         Label nameLabel = new Label("Name: ");
@@ -89,62 +96,156 @@ public class AddStudent extends GenericGUI {
         });
 
         submitBtn.setOnAction((event) -> {
+            // reset previous errors (for multiple attempts)
+            emailLabel.setText("Email: ");
+            emailLabel.setStyle(null);
+            zipCodeLabel.setText("Zip code: ");
+            zipCodeLabel.setStyle(null);
+            nameLabel.setText("Name: ");
+            nameLabel.setStyle(null);
+            birthdateLabel.setText("Birthdate: ");
+            birthdateLabel.setStyle(null);
+            addressLabel.setText("Address: ");
+            addressLabel.setStyle(null);
+            cityLabel.setText("City: ");
+            cityLabel.setStyle(null);
+            countryLabel.setText("Country: ");
+            countryLabel.setStyle(null);
+
+            Boolean isValidStudent = true;
+            String strDate = "";
+
+            // Check mail
+            if (MailValidation.validateMailAddress(emailInput.getText())) {
+                emailInput.setStyle("-fx-text-box-border: green");
+            } else {
+                emailLabel.setText("Email: Invalid Email!");
+                emailLabel.setStyle("-fx-text-fill: red");
+                emailInput.setStyle("-fx-text-box-border: red");
+                isValidStudent = false;
+            }
+
+            // Check firstname + lastname
+            if (NameValidation.validateName(nameInput.getText())) {
+                nameInput.setStyle("-fx-text-box-border: green");
+                String[] parts = nameInput.getText().split(" ");
+                String formattedName = "";
+                boolean hasSpace = false;
+                for (String x : parts) {
+                    if (!hasSpace) {
+                        formattedName += x.substring(0, 1).toUpperCase() + x.substring(1) + " ";
+                        hasSpace = true;
+                    } else {
+                        formattedName += x.substring(0, 1).toUpperCase() + x.substring(1);
+                    }
+
+                }
+                nameInput.setText(formattedName);
+            } else {
+                nameLabel.setText("Name: Invalid name! (Don't forget capital letters)");
+                nameLabel.setStyle("-fx-text-fill: red");
+                nameInput.setStyle("-fx-text-box-border: red");
+            }
+
+            // Check date
             try {
-                // reset previous errors (for multiple attempts)
+                strDate = String.format("%s-%s-%s", Integer.parseInt(yearInput.getText()),
+                        Integer.parseInt(monthInput.getText()), Integer.parseInt(dayInput.getText()));
+                String validateDate = String.format("%s-%s-%s", Integer.parseInt(dayInput.getText()),
+                        Integer.parseInt(monthInput.getText()), Integer.parseInt(yearInput.getText()));
+
+                if (DateValidation.validateDate(validateDate)) {
+                    dayInput.setStyle("-fx-text-box-border: green");
+                    monthInput.setStyle("-fx-text-box-border: green");
+                    yearInput.setStyle("-fx-text-box-border: green");
+                } else {
+                    birthdateLabel.setText("Birthdate: Invalid birthdate!");
+                    birthdateLabel.setStyle("-fx-text-fill: red");
+                    dayInput.setStyle("-fx-text-box-border: red");
+                    monthInput.setStyle("-fx-text-box-border: red");
+                    yearInput.setStyle("-fx-text-box-border: red");
+                    isValidStudent = false;
+                }
+            } catch (NumberFormatException e) {
+                birthdateLabel.setText("Birthdate: Invalid birthdate!");
+                birthdateLabel.setStyle("-fx-text-fill: red");
+                dayInput.setStyle("-fx-text-box-border: red");
+                monthInput.setStyle("-fx-text-box-border: red");
+                yearInput.setStyle("-fx-text-box-border: red");
+                isValidStudent = false;
+            }
+            // check location infromation
+            // Address
+            if (!addressInput.getText().equals("")) {
+                addressInput.setStyle("-fx-text-box-border: green");
+            } else {
+                addressLabel.setText("Address: Address is empty");
+                addressLabel.setStyle("-fx-text-fill: red");
+                addressInput.setStyle("-fx-text-box-border: red");
+                isValidStudent = false;
+            }
+            // Country
+            if (!countryInput.getText().equals("")) {
+                countryInput.setStyle("-fx-text-box-border: green");
+            } else {
+                countryLabel.setText("Country: Country is empty");
+                countryLabel.setStyle("-fx-text-fill: red");
+                countryInput.setStyle("-fx-text-box-border: red");
+                isValidStudent = false;
+            }
+            // City
+            if (!cityInput.getText().equals("")) {
+                cityInput.setStyle("-fx-text-box-border: green");
+            } else {
+                cityLabel.setText("City: City is empty");
+                cityLabel.setStyle("-fx-text-fill: red");
+                cityInput.setStyle("-fx-text-box-border: red");
+                isValidStudent = false;
+            }
+
+            // Check ZipCode
+            if (ZipCodeValidation.validateZipCode(zipCodeInput.getText())) {
+                zipCodeInput.setText(ZipCodeValidation.formatZipCode(zipCodeInput.getText()));
+                zipCodeInput.setStyle("-fx-text-box-border: green");
+            } else {
+                zipCodeLabel.setText("Zip code: Invalid zip code!");
+                zipCodeLabel.setStyle("-fx-text-fill: red");
+                zipCodeInput.setStyle("-fx-text-box-border: red");
+                isValidStudent = false;
+            }
+
+            // Add student
+            if (isValidStudent) {
+                studentDAO.addStudent(new Student(emailInput.getText(), nameInput.getText(), strDate,
+                        genderInput.getValue(), addressInput.getText(), cityInput.getText(), countryInput.getText(),
+                        zipCodeInput.getText()));
+
+                responseMsg.setText(nameInput.getText() + " has been added");
+                responseMsg.setStyle("-fx-text-fill: green");
+
+                emailInput.clear();
+                nameInput.clear();
+                dayInput.clear();
+                monthInput.clear();
+                yearInput.clear();
+                genderInput.setValue("Male");
+                addressInput.clear();
+                cityInput.clear();
+                countryInput.clear();
                 emailLabel.setText("Email: ");
                 emailLabel.setStyle(null);
                 zipCodeLabel.setText("Zip code: ");
                 zipCodeLabel.setStyle(null);
-
-                Boolean isValidStudent = true;
-
-                // Check mail
-                if (MailValidation.validateMailAddress(emailInput.getText())) {
-                    emailInput.setStyle("-fx-text-fill: green");
-                } else {
-                    emailLabel.setText("Email: Invalid Email!");
-                    emailLabel.setStyle("-fx-text-fill: red");
-                    emailInput.setStyle("-fx-text-box-border: red");
-                    isValidStudent = false;
-                }
-
-                // Check ZipCode
-                if (ZipCodeValidation.validateZipCode(zipCodeInput.getText())) {
-                    zipCodeInput.setText(ZipCodeValidation.formatZipCode(zipCodeInput.getText()));
-                    zipCodeInput.setStyle("-fx-text-fill: green");
-                } else {
-                    zipCodeLabel.setText("Zip code: Invalid zip code!");
-                    zipCodeLabel.setStyle("-fx-text-fill: red");
-                    zipCodeInput.setStyle("-fx-text-box-border: red");
-                    isValidStudent = false;
-                }
-                // Check if input is a number and make it correct date format
-                String strDate = String.format("%s-%s-%s", Integer.parseInt(yearInput.getText()),
-                        Integer.parseInt(monthInput.getText()), Integer.parseInt(dayInput.getText()));
-
-                // Add student
-                if (isValidStudent) {
-                    studentDAO.addStudent(new Student(emailInput.getText(), nameInput.getText(), strDate,
-                            genderInput.getValue(), addressInput.getText(), cityInput.getText(), countryInput.getText(),
-                            zipCodeInput.getText()));
-
-                    responseMsg.setText(nameInput.getText() + " has been added");
-                    responseMsg.setStyle("-fx-text-fill: green");
-
-                    emailInput.clear();
-                    nameInput.clear();
-                    dayInput.clear();
-                    monthInput.clear();
-                    yearInput.clear();
-                    genderInput.setValue("Male");
-                    addressInput.clear();
-                    cityInput.clear();
-                    countryInput.clear();
-                }
-
-            } catch (NumberFormatException e) {
-                responseMsg.setText("Date is not a number");
-                responseMsg.setStyle("-fx-text-fill: red");
+                nameLabel.setText("Name: ");
+                nameLabel.setStyle(null);
+                birthdateLabel.setText("Birthdate: ");
+                birthdateLabel.setStyle(null);
+                addressLabel.setText("Address: ");
+                addressLabel.setStyle(null);
+                cityLabel.setText("City: ");
+                cityLabel.setStyle(null);
+                countryLabel.setText("Country: ");
+                countryLabel.setStyle(null);
             }
         });
 
--- src/Logic/Validation/DateValidation.java
+++ src/Logic/Validation/DateValidation.java
@@ -1,30 +1,36 @@
 package Logic.Validation;
 
 public class DateValidation {
-    
+
     public static boolean validateDate(String date) {
 
-        if (date.length() < 10 && date.length() > 10){
+        // Check length
+        if (date.length() < 10 || date.length() > 10) {
             return false;
         }
-        
+
         String dateParts[] = date.split("-");
 
-        if (dateParts.length < 3 && dateParts.length > 3){
+        if (dateParts.length < 3 || dateParts.length > 3) {
             return false;
         }
-        
+
         int day = Integer.valueOf(dateParts[0]);
         int month = Integer.valueOf(dateParts[1]);
         int year = Integer.valueOf(dateParts[2]);
-        System.out.println("days: " + day + " month: " + month + " year: " + year);
+        // System.out.println("days: " + day + " month: " + month + " year: " + year);
 
-        //* @subcontract 31 days in month
-        if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && (day < 1 || day > 31)) {
+        if (1 < month && month > 12) {
+            return false;
+        }
+
+        // * @subcontract 31 days in month
+        if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+                && (day < 1 || day > 31)) {
             System.out.println("first");
             return false;
         }
-        // * @subcontract 30 days in month 
+        // * @subcontract 30 days in month
         if ((month == 4 || month == 6 || month == 9 || month == 11) && (day < 1 || day > 30)) {
             System.out.println("second");
             return false;
@@ -35,11 +41,11 @@ public class DateValidation {
             return false;
         }
         // * @subcontract 28 days in month
-        if (month == 2 && (day < 1 || day > 28) && (year % 4 != 0 || (year % 100 == 0 && year % 400 != 0))){
+        if (month == 2 && (day < 1 || day > 28) && (year % 4 != 0 || (year % 100 == 0 && year % 400 != 0))) {
             System.out.println("fourth");
             return false;
         }
-        
+
         return true;
     }
 }
