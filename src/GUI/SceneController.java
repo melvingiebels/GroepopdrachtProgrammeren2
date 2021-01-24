d71f2a0f70dd268e1ff3ce38ec56bb4e17f5a18c
Pieter Bakker <18117805@student.hhs.nl>
2021-01-24 18:12:27 +0100

    altered icon image to look better

--- src/GUI/SceneController.java
+++ src/GUI/SceneController.java
@@ -9,7 +9,7 @@ public class SceneController extends Application{
     @Override
     public void start(Stage window) throws Exception {
         MainMenu mainMenuScene = new MainMenu();
-        window.getIcons().add(new Image(SceneController.class.getResourceAsStream("./Media/arno.png")));
+        window.getIcons().add(new Image(SceneController.class.getResourceAsStream("./Media/arnocropped.png")));
         
         window.setScene(mainMenuScene.getScene(window));
         window.show();        
